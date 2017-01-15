b11e4699a457717f96a366835e489456c6022c3a
Gustavo Brian <gustavo.brian@sembo.se>
2017-01-15 22:39:42 +0100

    Fixing name

--- google/index.js
+++ google/index.js
@@ -1,5 +1,5 @@
 require('@google/cloud-debug').start();
-exports.helloGET = function (req, res) {
+exports.b2b-xml-provider-proxy = function (req, res) {
 	console.log(req);
 	res.send('Hello World!...1');
 };
