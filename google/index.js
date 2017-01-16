15517bd9971a7acee654fd86a1afa3f5486bcfef
Gustavo Brian <gustavo.brian@sembo.se>
2017-01-16 05:35:16 +0100

    xml2json basic implementation

--- google/index.js
+++ google/index.js
@@ -1,5 +1,32 @@
 require('@google/cloud-debug').start();
+var parseString = require('xml2js').parseString;
+var request = require('request');
+var compression = require('compression')();
+
+function xml2json(xml, callback){
+	parseString(xml, function(err, json){ callback(json) });
+}
+function downloadXml(url, callback){
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			callback(body) 
+		}else{
+			callback("error");
+		}
+	})
+}
+
 exports.b2b_xml_provider_proxy = function b2b_xml_provider_proxy(req, res) {
-	console.log(req);
-	res.send('Hello World!...1');
+	res.set('Content-Type', 'application/json');
+	compression(req, res, function() {
+		doTheJob(req, res);
+	});
+}
+function doTheJob(req, res){
+	var url = req.query.url;
+	var callback = function(data){ res.send(data); };
+	downloadXml(url, 
+		function(xml){ 
+			xml2json(xml, callback)
+		});
 };
